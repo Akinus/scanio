f9869d08292da78c9fc66836d0bed2183ec5875e
Akinus <gabriel.akonom@gmail.com>
2020-10-18 17:14:11 -0700

    Improvements to command line display

--- scanio.py
+++ scanio.py
@@ -718,7 +718,7 @@ if __name__ == '__main__':
                 printall(addy)
 
     except KeyboardInterrupt:
-        print('\n\n\t\t!!! SCAN INTERRUPTED !!! Retrieving Current Results...\n')
+        print('\n\n\t\t!!! SCAN INTERRUPTED !!!\n')
         time.sleep(0.2)
         logVars = sortXML(printnet)
         if logVars == 1:
@@ -733,7 +733,7 @@ if __name__ == '__main__':
     currcount.value = totalscans
     update_progress()
     time.sleep(0.3)
-    # print('\n\n\t\t*** SCAN COMPLETE *** Retrieving Final Results...')
+    print('\n\n\t\t*** SCAN COMPLETE *** ')
     logVars = sortXML(printnet)
     if logVars == 1:
         print('\nNo Hosts Found...')
