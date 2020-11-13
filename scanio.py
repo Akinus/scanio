3e222768a95d02af96ba7230cc3425b9b40d0cb6
Akinus <gabriel.akonom@gmail.com>
2020-11-13 15:29:41 -0800

    Gobuster Scan Testing

--- scanio.py
+++ scanio.py
@@ -694,8 +694,8 @@ class scanjobs(object):
             
             if enum:
                 if search('http', banner):
-                    httpaddy = 'http://'+str(addy)+':'
-                    gobuster = self.gobusterScan(httpaddy, tp)
+                    domain = 'http://'
+                    gobuster = self.gobusterScan(domain, addy, tp)
                     self.addGobuster(addy, gobuster)
 
             io.sortXML(io(), addy)
@@ -743,15 +743,15 @@ class scanjobs(object):
             out = ''
         return out
 
-    def gobusterScan(self, addy, port):
+    def gobusterScan(self, domain, addy, port):
         try:
-            tcp_args = 'timeout 300 bash -c "gobuster dir -u '+addy+':'+port+' -t 35 --wordlist="/usr/share/seclists/Discovery/Web-Content/directory-list-2.3-small.txt"'
+            tcp_args = 'timeout 300 bash -c "gobuster dir -u '+domain+addy+':'+port+' -t 35 --wordlist=\'/usr/share/seclists/Discovery/Web-Content/directory-list-2.3-small.txt\'"'
             tcp_res = sub.Popen(tcp_args, stdout = sub.PIPE, stderr = sub.PIPE, universal_newlines = True, shell = True)
             tcp_res.wait(300)
             out, err = tcp_res.communicate()
             tcp_res.kill()
         except (Exception, KeyboardInterrupt, SystemExit):
-            out = 'NMAP-Error.'
+            out = 'gobuster-Error.'
             # raise Exception
 
         if search('concurrent connection', out):
